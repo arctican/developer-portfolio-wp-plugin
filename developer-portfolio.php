6905753ed9a99cc815e6ccca856d96dc71c8c3a4
Adam Rogers <adam@arcticanaudio.com>
2016-07-15 17:46:40 +0200

    Initial Commit

---
+++ developer-portfolio.php
@@ -0,0 +1,30 @@
+<?php
+/*
+Plugin Name: Developer Portfolio
+Plugin URI:  http:/arcticanaudio.com
+Description: A portfolio plugin designed for developers
+Version:     0.0.1
+Author:      Arctican Audio
+Author URI:  http:/arcticanaudio.com
+License:     GPL2
+
+Developer Portfolio is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+Developer Portfolio is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Developer Portfolio. If not, see http://www.gnu.org/licenses/gpl.html
+*/
+
+
+
+
+
+
+?>
